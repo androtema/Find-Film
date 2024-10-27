fc6a1bcd0c10d0661fab6c73979016606add1d07
Artem L. <tema.luchnikov@gmail.com>
2024-10-27 16:52:24 +0700

    Homework 20-21 Added: Material components, menu navigation, Toolbar, black theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,10 @@ android {
             )
         }
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -42,7 +46,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.material.v1120)
+
 }
--- app/src/main/java/com/temalu/findfilm/MainActivity.kt
+++ app/src/main/java/com/temalu/findfilm/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.temalu.findfilm
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.AppBarLayout
+import com.temalu.findfilm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,69 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> false
+            }
+
+        }
+
+
+        /*val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener{
+            Toast.makeText(applicationContext, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener{
+            Toast.makeText(applicationContext, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+
+        val button3: Button = findViewById(R.id.button3)
+        button3.setOnClickListener{
+            Toast.makeText(applicationContext, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+
+        val button4: Button = findViewById(R.id.button4)
+        button4.setOnClickListener{
+            Toast.makeText(applicationContext, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+
+        val button5: Button = findViewById(R.id.button5)
+        button5.setOnClickListener{
+            Toast.makeText(applicationContext, "Настройки", Toast.LENGTH_SHORT).show()
+        }*/ //кликер кнопок и toast
     }
 }
